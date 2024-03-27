059d88046312cb9dd7e5bf6bcdd27cea9c558248
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-03-27 21:03:35 +0300

    Почти сохраняет файл

--- phone_book.py
+++ phone_book.py
@@ -1,3 +1,109 @@
+import json
+
+
 # на Отлично в одного человека надо сделать консольное приложение Телефонный справочник с внешним хранилищем информации,
 # и чтоб был реализован основной функционал - просмотр, сохранение, импорт, поиск, удаление, изменение данных.
+def rules():
+    print('\n Команды работы со справочником \n'
+          '1 - просмотр всех конткактов \n'
+          '2 - создание новых конткактов \n'
+          '3 - импорт \n'
+          '4 - поиск \n'
+          '5 - удаление данных \n'
+          '6 - изменения данных \n'
+          '7 - изменить подсказки\n'
+          '8 - завершение работы программы')
 
+
+def rules_s():
+    print('\n Список команд \n'
+          '1 - добавить или изменить имя ползователя \n'
+          '2 - добавить или изменить номера телефонов \n'
+          '3 - добавить или изменить дату рождения \n'
+          '4 - добавить или изменить описание \n'
+          '5 - вывести данные в терминале\n'
+          '6 - сохранить данные \n'
+          '7 - вернутся назад \n')
+
+
+def err():
+    print("\n Такой команды не существует \n"
+          "нажмите Enter что-бы продолжить")
+    input('-> ')
+    print()
+
+
+def conti_save(char, name):
+    with open("phone_db.json", "r+w", encoding="UTF-8") as file:
+        records = json.load(file)
+        records[name] = char[name].copy()
+        json.dump(records, file, ensure_ascii=False)
+    input('\nДанные были успешно сохранены\n-> ')
+
+
+def save_data():
+    stock = {'/non_data': {'phone_numb': [], 'b_date': 'non_data', 'description': 'non_data'}}
+    char = stock.copy()
+    name = '/non_data'
+    while True:
+        rules_s()
+        cmd = input('-> ')
+        if cmd == '1':
+            char[name := input('\nВведите имя\n-> ')] = char.pop(name)
+        elif cmd == '2':
+            numbers = input('\nВведите номера через прробел\n!Внимание! внутри номера не должно быть пробелов\n'
+                            '+7 999 999 99 99 X \n+7-999-999-99-99 V\n-> ').split()
+            char[name]['phone_numb'] = numbers
+        elif cmd == '3':
+            char[name]['b_date'] = input('\nВведите дату рождения\n-> ')
+        elif cmd == '4':
+            char[name]['description'] = input('\nВведите описание контакта\n-> ')
+        elif cmd == '5':
+            print()
+            print(f'Имя: {name}\nТелефон: {char[name]['phone_numb']}\nДата рождения: {char[name]['b_date']}\n'
+                  f'Описание: {char[name]['description']}')
+            input('Enter что-бы продолжить\n-> ')
+        elif cmd == '6':
+            if name == '/non_data':
+                input('\nВы не ввели имя\n-> ')
+            elif len(char[name]['phone_numb']) == 0:
+                if input('\nВы не ввели номер телефона\n1 - Все равно сохранить\n2 - Отмена\n-> ') == '1':
+                    conti_save(char, name)
+            elif stock == char:
+                input('\nВы ничего не ввели\n-> ')
+            else:
+                conti_save(char, name)
+
+        elif cmd == '7':
+            if stock == char:
+                return
+            else:
+                if input('\nНе сохраненые данные будут утерены\nПродолжить?\n1 - Продолжить\n2 - Отмена\n-> ') == '1':
+                    return
+        else:
+            err()
+
+
+while True:
+    rules()
+    n = input('-> ')
+    if n == '1':
+        pass
+    elif n == '2':
+        save_data()
+    elif n == '3':
+        print('Эта функция еще не реалезована')
+    elif n == '4':
+        pass
+    elif n == '5':
+        pass
+    elif n == '6':
+        pass
+    elif n == '7':
+        pass
+    elif n == '8':
+        break
+    else:
+        err()
+
+print('\n Работа программы прекращена')
