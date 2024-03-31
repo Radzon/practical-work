e6bc6d62d5ed82d46dadb41b1e3bec991d3fd201
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-03-31 17:14:18 +0300

    31.03

--- tg_bot.py
+++ tg_bot.py
@@ -1,16 +1,30 @@
 import telebot
+from telebot import types
 
-# токен скрыл на всякий случай
+
 with open('C:/Users/Admin/Desktop/tg_token.txt', encoding="UTF-8") as file_in:
-    token = file_in.read()
-API_TOKEN = token
+    token = file_in.read().strip()
 
-bot = telebot.TeleBot(API_TOKEN)
-
-
-@bot.message_handler(commands=['start'])
-def main(massage):
-    bot.send_message(massage.chat.id, 'Привет это мой первый бот')
+bot = telebot.TeleBot(token)
 
 
-bot.polling()
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://google.com')
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+    markup.row(btn2, btn3)
+    bot.reply_to(message, 'Классное фото!', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_massage(callback):
+    if callback.data == 'delete':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == 'edit':
+        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
+
+
+bot.infinity_polling()
