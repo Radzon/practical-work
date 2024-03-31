71bc4971e8d4fe4c319d3ee958b9b41c1b7a7e9e
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-03-31 21:40:22 +0300

    Создал класс игрока

--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,48 @@ import telebot
 from telebot import types
 
 
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.level = 1
+        self.money = 0
+        self.experience = 0
+        self.skills = {}
+        self.career_achievements = []
+        self.score = 100
+
+    def gain_experience(self, exp):
+        while exp >= self.score:
+            self.level_up()
+        self.experience += exp
+
+    def give_money(self, m):
+        self.money += m
+
+    def level_up(self):
+        self.level += 1
+        self.score *= 2
+
+    def learn_skill(self, skill_name):
+        if skill_name not in self.skills:
+            self.skills[skill_name] = 1
+        elif self.skills[skill_name] < 5:
+            self.skills[skill_name] += 1
+
+    def add_career_achievement(self, achievement):
+        self.career_achievements.append(achievement)
+
+    def get_player_info(self):
+        player_info = f"Имя игрока: {self.name}\nДеньги: {self.money}\nУровень: {self.level}\nОпыт: {self.experience}\n"
+        player_info += "Навыки:\n"
+        for skill, level in self.skills.items():
+            player_info += f"- {skill}: {level}\n"
+        player_info += "Достижения в карьере:\n"
+        for achievement in self.career_achievements:
+            player_info += f"- {achievement}\n"
+        return player_info
+
+
 with open('C:/Users/Admin/Desktop/tg_token.txt', encoding="UTF-8") as file_in:
     API_TOKEN = file_in.read().strip()
 
