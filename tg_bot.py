b57f1517840b724b01b167ba6f84d099c1269c91
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-03-31 23:12:31 +0300

    есть выбор работы и работа, но с багами

--- tg_bot.py
+++ tg_bot.py
@@ -11,6 +11,8 @@ class Player:
         self.skills = {}
         self.career_achievements = []
         self.score = 100
+        self.job = 'Нет работы'
+        self.salary = 0
 
     def gain_experience(self, exp):
         while exp >= self.score:
@@ -24,6 +26,10 @@ class Player:
         self.level += 1
         self.score *= 2
 
+    def new_job(self, comp, sal):
+        self.job = comp
+        self.salary = sal
+
     def learn_skill(self, skill_name):
         if skill_name not in self.skills:
             self.skills[skill_name] = 1
@@ -33,11 +39,21 @@ class Player:
     def add_career_achievement(self, achievement):
         self.career_achievements.append(achievement)
 
+    def get_level(self):
+        return self.level
+
+    def get_job(self):
+        return self.job
+
+    def get_salary(self):
+        return self.salary
+
     def get_player_info(self):
         player_info = f"Имя игрока: {self.name}\nДеньги: {self.money}\nУровень: {self.level}\nОпыт: {self.experience}\n"
         player_info += "Навыки:\n"
         for skill, level in self.skills.items():
             player_info += f"- {skill}: {level}\n"
+        player_info += f'Место роботы: {self.job}\n'
         player_info += "Достижения в карьере:\n"
         for achievement in self.career_achievements:
             player_info += f"- {achievement}\n"
@@ -49,42 +65,82 @@ with open('C:/Users/Admin/Desktop/tg_token.txt', encoding="UTF-8") as file_in:
 
 bot = telebot.TeleBot(API_TOKEN)
 
-SELECT_ACTION, SELECT_JOB, SELECT_TECHNOLOGY, MAIN_MENU = range(4)
 
 # Клавиатура для выбора действия
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-markup.row('Устройство на работу', 'Изучение новых технологий')
-markup.row('Покупка оборудования', 'Работа над проектами')
-markup.row('Участие в событиях', 'Развитие карьеры')
+markup.row('Работать', 'Устроится на работу')
+markup.row('Покупка оборудования', 'Изучение новых технологий')
+markup.row('Статус', 'Развитие карьеры')
 markup.row('Достижения')
 
+markup_jobs = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup_jobs.row('NeuroCodex', 'VirtuoSphere')
+markup_jobs.row('DigitalFusion', 'CyberSpires')
+markup_jobs.row('HyperTech', 'Solutions')
+markup_jobs.row('InnovaTechnosis')
 
-# Обработчик команды /start
+company = {'NeuroCodex': [1, 150], 'VirtuoSphere': [3, 200], 'DigitalFusion': [5, 350],
+           'CyberSpires': [8, 500], 'HyperTech': [8, 500], 'Solutions': [15, 900], 'InnovaTechnosis': [20, 1500]}
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
+    user = Player(message.from_user.first_name)
     bot.send_message(message.chat.id,
                      f"Привет, {message.from_user.first_name}! Добро пожаловать в игру Code tycoon!\n"
                      "Выберите действие:",
                      reply_markup=markup)
-    bot.register_next_step_handler(message, select_action)
+    bot.register_next_step_handler(message, select_action, user)
 
 
-# Обработчик действий
-def select_action(message):
+@bot.message_handler(commands=['site'])
+def site(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="GitHub",
+                                            url="https://github.com/Radzon/practical-work/blob/main/tg_bot.py")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Исходный код", reply_markup=keyboard)
+
+
+def select_action(message, user):
     text = message.text
-    if text == 'Устройство на работу':
-        bot.send_message(message.chat.id, "Выберите, в какую компанию хотите устроиться:",
-                         reply_markup=types.ReplyKeyboardRemove())
-        bot.register_next_step_handler(message, select_job)
-    elif text == 'Изучение новых технологий':
-        bot.send_message(message.chat.id, "Выберите технологию, которую хотите изучить:")
-        bot.register_next_step_handler(message, select_technology)
-    # Другие действия обрабатываются аналогично
+    if text == 'Работать':
+        if user.get_job() == 'Нет работы':
+            bot.send_message(message.chat.id, "Вы вы еще не устроились на работу")
+        else:
+            bot.send_message(message.chat.id, f"Вы заработали:\n{user.get_salary()} доларов\n100 опыта")
+    elif text == 'Устроится на работу':
+        bot.send_message(message.chat.id, "Выберите, в какую компанию хотите устроиться:"
+                                          "\nNeuroCodex - Минимальный уровень: 1 | Зарплата: 150$"
+                                          "\nVirtuoSphere - Минимальный уровень: 3 | Зарплата: 200$"
+                                          "\nDigitalFusion - Минимальный уровень: 5 | Зарплата: 350$"
+                                          "\nCyberSpires - Минимальный уровень: 8 | Зарплата: 500$"
+                                          "\nHyperTech - Минимальный уровень: 10 | Зарплата: 750$"
+                                          "\nSolutions - Минимальный уровень: 15 | Зарплата: 900$"
+                                          "\nInnovaTechnosis - Минимальный уровень: 20 | Зарплата: 1500$",
+                         reply_markup=markup_jobs)
+        bot.register_next_step_handler(message, select_job, user)
+    elif text == 'Статус':
+        bot.send_message(message.chat.id, user.get_player_info())
+    bot.register_next_step_handler(message, select_action, user)
 
 
-def main():
-    bot.polling()
+def select_job(message, user):
+    if company[message.text][0] <= user.get_level():
+        user.new_job(message.text, company[message.text][1])
+        text = 'Вы устроились на работу'
+    else:
+        text = 'Ваш уровень слишком мал'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
+    bot.register_next_step_handler(message, select_action, user)
 
 
-if __name__ == '__main__':
-    main()
+def work_on_job():
+    pass
+
+
+def select_technology(message):
+    pass
+
+
+bot.polling()
